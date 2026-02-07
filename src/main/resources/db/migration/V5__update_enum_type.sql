8e4befc66746862da14cb0f16de44d38b337e157
Saffat Zabin <1905060@ugrad.cse.buet.ac.bd>
2026-02-07 22:08:09 +0600

    feat: added all basic repositories

--- src/main/resources/db/migration/V5__update_enum_type.sql
+++ src/main/resources/db/migration/V5__update_enum_type.sql
@@ -1,3 +1,3 @@
 -- Renaming the property_status enum value new to brand_new, because new is a reserved keyword in Java
 
-ALTER TYPE listing_condition RENAME VALUE "new" TO "brand_new"
+ALTER TYPE listing_condition RENAME VALUE 'new' TO 'brand_new';
