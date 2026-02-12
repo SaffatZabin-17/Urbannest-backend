fb35b00e39df18c4947c9cf72ebb36596ee9c93c
Saffat Zabin <1905060@ugrad.cse.buet.ac.bd>
2026-02-12 23:11:29 +0600

    update: s3 config name

--- src/main/java/com/example/urbannest/config/S3Config.java
+++ src/main/java/com/example/urbannest/config/S3Config.java
@@ -3,6 +3,7 @@ package com.example.urbannest.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
@@ -11,13 +12,13 @@ import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
 public class S3Config {
-    @Value("${aws.s3.access-key-id}")
+    @Value("${aws.access-key-id}")
     private String accessKeyId;
-    @Value("${aws.s3.access-key-secret}")
+    @Value("${aws.access-key-secret}")
     private String accessKeySecret;
     @Value("${aws.s3.bucket-name}")
     private String bucketName;
-    @Value("${aws.s3.region}")
+    @Value("${aws.region}")
     private String region;
 
     @Bean
